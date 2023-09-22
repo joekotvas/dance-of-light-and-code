09741376abd3ce329dfef89203bf2a9261247562
joekotvas <joseph.kotvas@gmail.com>
2023-09-21 20:02:05 -0500

    Lots of improvements, esp on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BiRefresh } from 'react-icons/bi'
 
 import haikuData from './haiku-data'
 
@@ -6,7 +7,8 @@ const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null)
   const [haiku, setHaiku] = useState([])
 
-  const composeHaiku = (category) => {
+  const composeHaiku = (category = selectedCategory) => {
+    setSelectedCategory(category)
     const getRandomLine = (lines) =>
       lines[Math.floor(Math.random() * lines.length)]
 
@@ -24,24 +26,33 @@ const App = () => {
 
   return (
     <div className="app">
+      <div className="app-headings-container">
+        <h1>Dance of Light and Code</h1>
+        <h2>AI Haiku Composer</h2>
+      </div>
       <div className="container">
         <div className="categories">
-          {window.innerWidth > 580 ? (
-            Object.keys(haikuData).map((category) => (
+          <div className="buttons">
+            {Object.keys(haikuData).map((category) => (
               <button key={category} onClick={() => composeHaiku(category)}>
                 {category}
               </button>
-            ))
-          ) : (
-            <select onChange={(e) => composeHaiku(e.target.value)}>
-              <option value="">Select a category</option>
-              {Object.keys(haikuData).map((category) => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          )}
+            ))}
+          </div>
+          <select
+            value={selectedCategory}
+            onChange={(e) => composeHaiku(e.target.value)}
+          >
+            <option value="">Select a category</option>
+            {Object.keys(haikuData).map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <button className="button--refresh" onClick={() => composeHaiku()}>
+            <BiRefresh />
+          </button>
         </div>
         <div className="haiku-wrapper">
           <div className="haiku">
@@ -53,6 +64,9 @@ const App = () => {
           </div>
         </div>
       </div>
+      <footer>
+        © 2023 <a href="https://josephkotvas.com/">Joseph Kotvas</a>
+      </footer>
     </div>
   )
 }
